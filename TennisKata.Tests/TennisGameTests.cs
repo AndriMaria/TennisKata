0f45a0f5ff4af6ed852d5403011685a7f2e7af72
AndriMaria <152691046+AndriMaria@users.noreply.github.com>
2026-08-17 13:53:40 +0300

    Green/Red test for 3-0,0-3,0-2,2-0,1-2,2-1

--- TennisKata.Tests/TennisGameTests.cs
+++ TennisKata.Tests/TennisGameTests.cs
@@ -35,6 +35,46 @@ public class TennisTests
       Assert.Equal("Love-Fifteen", result);
     }
     [Fact]
+    public void LoveThirtyResult()
+    {
+      //arrange
+      var game = new TennisKata();
+      //acttest
+      var result = game.GiveScore(0,2);
+      //assert
+      Assert.Equal("Love-Thirty", result);
+    }
+    [Fact]
+    public void ThirtyLoveResult()
+    {
+      //arrange
+      var game = new TennisKata();
+      //acttest
+      var result = game.GiveScore(2,0);
+      //assert
+      Assert.Equal("Thirty-Love", result);
+    }
+    [Fact]
+    public void FortyLoveResult()
+    {
+      //arrange
+      var game = new TennisKata();
+      //acttest
+      var result = game.GiveScore(3,0);
+      //assert
+      Assert.Equal("Forty-Love", result);
+    }
+    [Fact]
+    public void LoveFortyResult()
+    {
+      //arrange
+      var game = new TennisKata();
+      //acttest
+      var result = game.GiveScore(0,3);
+      //assert
+      Assert.Equal("Love-Forty", result);
+    }
+    [Fact]
     public void FifteenFifteenResults()
   {
     //arrange
@@ -44,13 +84,33 @@ public class TennisTests
       //assert
       Assert.Equal("Fifteen-Fifteen", result);
   }
+   [Fact]
+    public void FifteenThirtyResults()
+  {
+    //arrange
+      var game = new TennisKata();
+      //acttest
+      var result = game.GiveScore(1,2);
+      //assert
+      Assert.Equal("Fifteen-Thirty", result);
+  }
+  [Fact]
+    public void ThirtyFifteenResults()
+  {
+    //arrange
+      var game = new TennisKata();
+      //acttest
+      var result = game.GiveScore(2,1);
+      //assert
+      Assert.Equal("Thirty-Fifteen", result);
+  }
   [Fact]
   public void ThirtyThirtyResults()
   {
     //arrange
       var game = new TennisKata();
       //acttest
-      var result = game.GiveScore(1,1);
+      var result = game.GiveScore(2,2);
       //assert
       Assert.Equal("Thirty-Thirty", result);
   }
